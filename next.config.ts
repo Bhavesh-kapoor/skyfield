1527f6b9c848873e152caafc841689e15743b113
Kunal Verma <vkunal591@gmail.com>
2025-03-04 19:01:46 +0530

    add ip in next config

--- next.config.ts
+++ next.config.ts
@@ -11,6 +11,10 @@ const nextConfig: NextConfig = {
         protocol: "http",
         hostname: "localhost",
       },
+      {
+        protocol: "http",
+        hostname: "62.72.29.120",
+      },
     ],
   },
 
